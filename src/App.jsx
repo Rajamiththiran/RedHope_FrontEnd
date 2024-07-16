fe1c3c0891d26522effc3a15d9d0436e7f571423
Rajamiththiran <rjmithu7@gmail.com>
2024-07-16 15:39:07 +0530

    some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { Route, Routes } from "react-router-dom";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import DonorRegister from "./components/donor_register";
+import NavBar from "./components/nav_bar";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/donor-register" element={<DonorRegister />} />
+        {/* Add other routes here */}
+      </Routes>
+
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
