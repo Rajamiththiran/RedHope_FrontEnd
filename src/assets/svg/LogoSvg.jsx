c003d12f136fd04f9e27af315d1309fed36aacd2
Rajamiththiran <rjmithu7@gmail.com>
2024-07-16 15:38:08 +0530

    Assets added

---
+++ src/assets/svg/LogoSvg.jsx
@@ -0,0 +1,18 @@
+const LogoSvg = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 64 64"
+    width="64"
+    height="64"
+  >
+    <g fill="none" fillRule="evenodd">
+      <path
+        fill="#E53935"
+        d="M32 2C17.72 2 6 13.72 6 28c0 6.05 2.352 11.535 6.178 15.647L32 62l19.822-18.353C55.648 39.535 58 34.05 58 28 58 13.72 46.28 2 32 2z"
+      />
+      <path fill="#1976D2" d="M28 19h8v8h8v8h-8v8h-8v-8h-8v-8h8z" />
+    </g>
+  </svg>
+);
+
+export default LogoSvg;
