1d7eef00bb192b3e0d913f57fcaf18cdef342cda
Rajamiththiran <rjmithu7@gmail.com>
2024-07-19 19:07:08 +0530

    api config created

---
+++ src/api.js
@@ -0,0 +1,13 @@
+// src/api.js
+import axios from "axios";
+
+const API_BASE_URL = "https://your-api-base-url.com/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
