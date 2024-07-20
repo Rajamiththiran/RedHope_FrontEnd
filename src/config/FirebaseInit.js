bc6269258118187eee31d7b8cd2b6aa41968098b
Rajamiththiran <rjmithu7@gmail.com>
2024-07-20 21:19:47 +0530

    firebase SDK updated

---
+++ src/config/FirebaseInit.js
@@ -0,0 +1,34 @@
+// src/config/firebaseInit.js
+
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { firebaseConfig } from "./FirebaseConfig";
+
+const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
+
+export const requestNotificationPermission = async () => {
+  try {
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      const token = await getToken(messaging, {
+        vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
+      });
+      console.log("FCM Token:", token);
+      return token;
+    } else {
+      console.log("Notification permission denied");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error requesting notification permission:", error);
+    return null;
+  }
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      resolve(payload);
+    });
+  });
